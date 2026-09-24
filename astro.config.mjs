49659346f3b81ea3bc5db3e28293b07efc4424b0
Aldy Azarya <azarya.dev@gmail.com>
2026-09-24 21:48:06 +0700

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://hire.aldyazarya.dev',
   integrations: [sitemap()],
-  build: { inlineStylesheets: 'auto' },
+  build: { inlineStylesheets: 'always' },
 });
